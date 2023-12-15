f7a443b1e44c2f44351c0a1cf00a2635e424555d
Alena Sharai <exultantislupus@gmail.com>
2023-12-16 00:34:51 +0400

    feat: solve tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,55 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chainLinks: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainLinks.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    const chainLink = `( ${String(value)} )`;
+    this.chainLinks.push({ chainLink });
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    try {
+      if (
+        position > 0 &&
+        this.chainLinks[position - 1] &&
+        typeof position === 'number'
+      ) {
+        const newArray = this.chainLinks.filter(
+          (_, index) => index !== position - 1
+        );
+        this.chainLinks = [...newArray];
+        return this;
+      } else throw new Error("You can't remove incorrect link!");
+    } catch (err) {
+      console.error(err);
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const newArray = this.chainLinks.reverse();
+    this.chainLinks = [...newArray];
+
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const result = this.chainLinks.map((link) => link.chainLink).join('~~');
+    try {
+      throw new Error();
+    } catch (err) {
+      this.chainLinks = [];
+    } finally {
+      return result;
+    }
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
